fa20527d56a3444abd5f54afb022ae6384473f2c
Hendi Santika <hendisantika@gmail.com>
2018-07-18 06:26:31 +0700

    #2 - Set Up project

---
+++ src/main/java/com/hendisantika/springtransaction/SpringTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionApplication.class, args);
+    }
+}
